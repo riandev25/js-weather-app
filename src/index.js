195c9a4db33ef96d2fe9dbdb2771b55733bbff70
Rian Miguel Engracia <engraciarian@gmail.com>
2022-08-09 23:09:58 +0800

    improve responsiveness

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import currentGeoWeather from "./geolocation";
 
 const buttons = document.querySelectorAll("[data-carousel-button]");
 const inputLocation = document.querySelector("[data-input-location]");
-const tempConversion = document.querySelector("[data-conversion]");
+
+currentGeoWeather.viaGeoLocation();
 
 inputLocation.addEventListener("keyup", (e) => {
   if (e.key === "Enter") {
@@ -28,18 +29,3 @@ buttons.forEach((button) => {
     delete activeSlide.dataset.active;
   });
 });
-
-tempConversion.addEventListener("click", () => {
-  const offset = 1;
-  const slides = document.querySelector(".first-section");
-
-  const activeSlide = slides.querySelector("[data-active]");
-  let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-  if (newIndex < 0) newIndex = slides.children.length - 1;
-  if (newIndex >= slides.children.length) newIndex = 0;
-
-  slides.children[newIndex].dataset.active = true;
-  delete activeSlide.dataset.active;
-});
-
-currentGeoWeather.viaGeoLocation();
